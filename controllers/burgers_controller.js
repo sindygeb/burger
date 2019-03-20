a762a0123d01d8d556dcee1415c0d7a104689e92
Sindy <sindy@vidku.com>
2019-03-20 16:49:36 -0500

    finish project
    finished project

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,51 @@
+//Import express
+var express = require("express");
+//create the router
+var router = express.Router();
+
+// Import burger.js
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+        burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function() {
+        res.redirect("/");
+    });
+});
+
+router.put("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function() {
+        res.redirect("/");
+    });
+});
+
+router.delete("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function() {
+        res.redirect("/");
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Mushroom Swiss Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Impossible Burger (Vegan)", 0);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Bacon Cheeseburger", 0);
