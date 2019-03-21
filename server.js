f6444b9c6f03f0f69ce4f1574039df50ee2ba6bb
Sindy <sindy.gebhard@gmail.com>
2019-03-20 20:14:50 -0500

    change it up
    change it

--- server.js
+++ server.js
@@ -1,46 +1,28 @@
-// ==============================================================================
-// DEPENDENCIES
-// ==============================================================================
-
 var express = require("express");
-var bodyParser = require("body-parser");
-var path = require('path');
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// ==============================================================================
+var PORT = process.env.PORT || 8080;
 
 var app = express();
-var PORT = process.env.PORT || 3000;
 
-// ==============================================================================
-// CSS 
-// ==============================================================================
-app.use(express.static(path.join(__dirname, '/public')));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// ==============================================================================
-// MIDDLEWARE
-// ==============================================================================
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// ==============================================================================
-// HANDLEBAR
-// ==============================================================================
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// ==============================================================================
-// ROUTER
-// ==============================================================================
-var routes = require("./controllers/burgers_controller.js");
-app.use("/", routes);
 
-// ==============================================================================
-// LISTENER
-// ==============================================================================
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
+    // Log (server-side) when our server has started
     console.log('Burger Time App listening on PORT: ' + PORT);
 });
