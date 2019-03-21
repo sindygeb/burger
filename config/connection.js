c0c950ffbc8bd7af0dc7423da0672997ddccba1c
Sindy <sindy.gebhard@gmail.com>
2019-03-20 21:33:24 -0500

    update the connection
    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,18 @@
 // Set up MySQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "MilaBean417",
     database: "burgers_db"
 });
+}
 
-// Make the connection
-connection.connect(function(err) {
-if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-    }
-    console.log("connected as id: " + connection.threadId);
-});
-
-// Export connection for our ORM to use
+connection.connect();
 module.exports = connection;
