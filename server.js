a1c8ce97159522311802fe451f948de50e103889
Sindy <sindy.gebhard@gmail.com>
2019-03-20 19:25:21 -0500

    update it
    update server Heroku issue

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-
+//DEPENDENCIES
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
-
+//EXPRESS
 var app = express();
+var port = process.env.PORT || 3000;
 
+//CSS
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
+//MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 app.use(methodOverride("_method"));
 
+//HANDLEBARS
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//ROUTER
 var routes = require("./controllers/burgers_controller.js");
-
 app.use("/", routes);
 
+//LISTENER
 app.listen(port);
